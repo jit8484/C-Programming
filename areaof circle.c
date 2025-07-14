10bc33bf89812ff58bcaf1acdcccf24fb72f36a0
sujit Bhosale <163643360+jit8484@users.noreply.github.com>
2025-07-14 23:42:18 +0530

    Area of  find how to

---
+++ areaof circle.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+     float radius;
+    printf("Enter radius:");
+    scanf("%f",&radius);
+    float pi=3.1415;
+    float area=pi*radius*radius;
+    printf("The area of ciracle is %f\n",area);
+    
+
+    return 0;
+}
