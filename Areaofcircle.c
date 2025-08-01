d78c7c87076ecff2391ccc1c4cc30b71ac7a35bc
sujit Bhosale <163643360+jit8484@users.noreply.github.com>
2025-08-01 11:38:20 +0530

    finding the area of circle

---
+++ Areaofcircle.c
@@ -0,0 +1,14 @@
+/// finding the area of circle in user input 
+#include <stdio.h>
+
+int main() {
+    
+    float radius;
+    printf("Enter radius:");
+    scanf("%f",&radius);
+    float pi=3.1415;
+    float area=pi*radius*radius;
+    printf("The area of circle is:%f\n",area);
+
+    return 0;
+}
