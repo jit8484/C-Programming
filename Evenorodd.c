52d6ca04ced6391397920e4e43862d8e31835df7
sujit Bhosale <163643360+jit8484@users.noreply.github.com>
2025-08-13 18:58:01 +0530

    how to finding even and odd number in c programming

---
+++ Evenorodd.c
@@ -0,0 +1,19 @@
+//how to finding even and odd number in c programming
+
+#include<stdio.h>
+int main(){
+    int x;
+    printf("Enter a Number:");
+    scanf("%d",&x);
+    if(x%2==0){
+        printf("Even Number");
+
+    }
+     else{
+        printf("Odd Number");
+    }
+
+    return 0;
+
+
+}
