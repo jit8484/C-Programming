77ebbe7085f0b19e15e2eb9f965735eb944c7dc4
sujit Bhosale <163643360+jit8484@users.noreply.github.com>
2025-08-15 16:42:29 +0530

    Taking input user to sum of 2 number

---
+++ Sumofno.c
@@ -0,0 +1,16 @@
+//Taking input user to sum of 2 number
+
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter Frist Number is :");
+    scanf("%d",&x);
+    printf("Enter Seconde Number is:");
+    scanf("%d",&y);
+
+    int sum=x+y;
+    printf("Sum of the Number that user is: %d\n",sum);
+    return 0;
+    
+
+}
