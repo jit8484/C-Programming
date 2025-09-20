37c65a123403e0fd40c02aa60c5dcf4839ade6a2
sujit Bhosale <163643360+jit8484@users.noreply.github.com>
2025-09-20 21:26:37 +0530

    Frist Number of the c

---
+++ frist.c
@@ -0,0 +1,17 @@
+//print the 1 to 100 number using the while loop
+//Both for loops and while loops are control flow structures in programming that allow you to repeatedly execute a block of code.
+//the for loop is used where we konw the number of iteration and while loop is used wher we dont konw the number of iteration .
+#include<stdio.h>
+int main(){
+  int i=1;
+    while(i<=100){
+        printf(" %d",i);
+        i++;
+    }
+    
+   
+   
+
+    return 0;
+
+}
