da043e1391f404e362bfdfaa318bb067a7ebf7d7
sujit Bhosale <163643360+jit8484@users.noreply.github.com>
2025-09-23 08:19:38 +0530

    Fibonacci  number of the programming

---
+++ FibonacciNo.c
@@ -0,0 +1,22 @@
+//// Fibonacci Series using a while loop in C
+// This program prints the Fibonacci series up to n terms.
+
+#include <stdio.h>
+
+int main() {
+    int n, first = 0, second = 1, next, i;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for(i = 0; i <=n; i++) {
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}
